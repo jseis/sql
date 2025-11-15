4b59a28dfb0b2b2b9b172fdd6633d2898911572e
jseis <jseisen@uwaterloo.ca>
2025-11-15 00:25:45 -0500

    completed some more prompts. DELETE prompt needs to be fixed.

--- 02_activities/assignments/Cohort_8/assignment2.sql
+++ 02_activities/assignments/Cohort_8/assignment2.sql
@@ -37,13 +37,13 @@ HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 
 --Approach one
 SELECT 
-	product_id
-	,vendor_id
-	,market_date
+	--product_id
+	--,vendor_id
+	market_date
 	,customer_id
-	,quantity
-	,cost_to_customer_per_qty
-	,transaction_time
+	--,quantity
+	--,cost_to_customer_per_qty
+	--,transaction_time
 	,dense_rank() OVER (
 		PARTITION BY customer_id ORDER BY market_date ASC
 	) AS customer_visit
@@ -84,7 +84,18 @@ WHERE x.customer_visit = 1;
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
-
+SELECT 
+	product_id,
+	vendor_id,
+	market_date,
+	customer_id,
+	quantity,
+	cost_to_customer_per_qty,
+	transaction_time,
+	count(product_id) OVER (
+		PARTITION BY customer_id, product_id
+	)
+FROM customer_purchases;
 
 
 -- String manipulations
@@ -99,11 +110,28 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
+SELECT 
+	product_id,
+	product_name,
+	product_size,
+	product_category_id,
+	product_qty_type,
+	LTRIM(SUBSTR(product_name, NULLIF(INSTR(product_name, '-'),0) + 1))
+FROM product;
+
 
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
 
-
+SELECT 
+	product_id,
+	product_name,
+	product_size,
+	product_category_id,
+	product_qty_type,
+	LTRIM(SUBSTR(product_name, NULLIF(INSTR(product_name, '-'),0) + 1))
+FROM product
+WHERE product_size REGEXP '[0-9]';
 
 -- UNION
 /* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
@@ -115,8 +143,31 @@ HINT: There are a possibly a few ways to do this query, but if you're struggling
 3) Query the second temp table twice, once for the best day, once for the worst day, 
 with a UNION binding them. */
 
-
-
+WITH x AS (
+	SELECT market_date, SUM(quantity*cost_to_customer_per_qty) as daily_sales
+	FROM customer_purchases as cp
+	GROUP BY market_date
+),
+y AS (
+	SELECT market_date
+	, daily_sales
+	,dense_rank() OVER (
+		ORDER BY daily_sales ASC
+	) as sales_rank_worst
+	,dense_rank() OVER (
+		ORDER BY daily_sales DESC
+	) as sales_rank_best
+	FROM x
+	
+)
+SELECT market_date, daily_sales
+FROM y
+WHERE sales_rank_worst = 1
+UNION
+SELECT market_date, daily_sales
+FROM y
+WHERE sales_rank_best = 1 
+;
 
 /* SECTION 3 */
 
@@ -132,6 +183,19 @@ How many customers are there (y).
 Before your final group by you should have the product of those two queries (x*y).  */
 
 
+SELECT 
+	v.vendor_name
+	,p.product_id
+	,count(c.customer_id)*5*x.original_price as sales_total
+FROM customer as c
+CROSS JOIN (
+	SELECT DISTINCT vendor_id, product_id, original_price FROM vendor_inventory
+) as x
+INNER JOIN vendor as v ON v.vendor_id = x.vendor_id
+INNER JOIN product as p ON p.product_id = x.product_id
+GROUP BY x.vendor_id, x.product_id;
+
+
 
 -- INSERT
 /*1.  Create a new table "product_units". 
@@ -139,18 +203,36 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
+DROP TABLE IF EXISTS product_units;
+
+CREATE TABLE product_units AS
+SELECT *, CURRENT_TIMESTAMP AS snapshot_timestamp 
+FROM product
+WHERE product_qty_type = 'unit';
+
 
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
-
+INSERT INTO product_units
+VALUES(24, 'Whole Wheat Bread', '1.5 lbs', 3, 'unit', CURRENT_TIMESTAMP); 
 
 -- DELETE
 /* 1. Delete the older record for the whatever product you added. 
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
-
+WITH x AS (
+	SELECT row_number() OVER (
+	PARTITION BY product_name
+	ORDER BY snapshot_timestamp ASC
+	) AS time_rank
+	FROM product_units
+)
+DELETE FROM product_units 
+WHERE product_name = 'Whole Wheat Bread'
+AND (SELECT time_rank FROM x) = 1;
+SELECT * FROM product_units;
 
 
 -- UPDATE
